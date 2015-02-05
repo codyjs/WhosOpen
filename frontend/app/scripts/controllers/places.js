a26c98b9eb419c293a49c7fb5f27ac9cd8e9fb1c
Cody Simms <c_s7@txstate.edu>
2015-02-05 10:21:30 -0600

    added input radius in miles, controller function to convert to meters

--- frontend/app/scripts/controllers/places.js
+++ frontend/app/scripts/controllers/places.js
@@ -7,13 +7,26 @@
  * # MainCtrl
  * Controller of the frontendApp
  */
+
+function milesToMeters(miles){
+  return miles/0.00062137;
+}
+
 var myapp = angular.module('frontendApp');
 
 myapp.controller('PlacesCtrl', function ($scope, ngGPlacesAPI) {
-  ngGPlacesAPI.nearbySearch({latitude:29.8890110, longitude:-97.9195770, openNow:true, types:['restaurant']}).then(
-  function(data){
-    $scope.places = data;
-    console.log(data);
-  });
+  $scope.radius = 1;
+  $scope.getPlaces = function(){
+    $scope.error='';
+    $scope.places='';
+    var radiusMeters = milesToMeters($scope.radius);
+    if (radiusMeters >= 50000){
+      $scope.error = 'ERROR: Radius Exceeded!';
+      return;
+    }
+    ngGPlacesAPI.nearbySearch({latitude:29.8890110, longitude:-97.9195770, openNow:true, types:['restaurant'], radius: radiusMeters}).then(function(data){
+      $scope.places = data;
+      console.log(data);
+    });
+  };
 });
-
